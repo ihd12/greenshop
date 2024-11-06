f05a4463221ce12e374b611a419d2052e600f088
DESKTOP-0S51HQG\admin <pink_candy02@naver.com>
2024-11-06 16:22:51 +0900

    경로수정

--- src/main/java/org/green/shop/controller/ItemController.java
+++ src/main/java/org/green/shop/controller/ItemController.java
@@ -19,13 +19,11 @@ import java.util.List;
 public class ItemController {
     private final ItemService itemService;
     @GetMapping("/admin/item/new")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemadd(Model model){
         model.addAttribute("itemDTO",new ItemDTO());
         return "item/itemadd";
     }
     @PostMapping("/admin/item/new")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemadd(ItemDTO itemDTO
             , @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model) throws Exception {
         //첫번째 이미지파일이 없을경우 다시 등록페이지로
@@ -39,7 +37,6 @@ public class ItemController {
     }
     //관리자 상품관리
     @GetMapping("/admin/itemlist")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemList(Model model, PageRequestDTO pageRequestDTO){
         PageResultDTO<ItemDTO, Item> result = itemService.getList(pageRequestDTO);
         model.addAttribute("result",result);
@@ -47,7 +44,6 @@ public class ItemController {
     }
     //상품하나조회
     @GetMapping("/admin/item/{itemId}")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemDetail(@PathVariable("itemId") Long itemId, Model model){
        ItemDTO itemDTO = itemService.getItem(itemId);
        model.addAttribute("itemDTO",itemDTO);
@@ -56,7 +52,6 @@ public class ItemController {
     }
     //상품수정
     @PostMapping("/admin/item/")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemUpdate(ItemDTO itemDTO
             , @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList)  {
         System.out.println("==============================");
@@ -71,7 +66,6 @@ public class ItemController {
     }
     //상품상세보기
     @GetMapping("/item/{itemId}")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net:80")
     public String itemDetailView(@PathVariable("itemId") Long itemId, Model model){
         ItemDTO itemDTO= itemService.getItem(itemId);
         model.addAttribute("itemDTO",itemDTO);
@@ -79,7 +73,6 @@ public class ItemController {
     }
     //상품리스트
     @GetMapping("/item/list")
-    @CrossOrigin(origins = "http://team4min.jinlabs.net/:80")
     public String itemList(PageRequestDTO pageRequestDTO, Model model){
         PageResultDTO<MainItemDTO,Object[]> result = itemService.getShopList(pageRequestDTO);;
         model.addAttribute("result",result);
